3e98aa6cfa30a67910b41f0f2040c3d8ee7d5396
Jan Behrens <dev@jpbehrens.de>
2021-12-06 14:19:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,278 @@
+/**
+ * JP BEHRENS <hallo@jpbehrens.de>
+ * MyLocalStorage
+ */
+
+class MyLocalStorage {
+    _maxSize = 5 * 1024 * 1024; // 5MB => 5242880 Bytes
+
+    // Work with primitive Datatypes
+
+    /** 
+     * @method set
+     * @description - set a new key-value pair Item in the localStorage
+     * @param {sting} key - set the localStorage Key
+     * @param {string|number} value - set the value of the Item
+     * @return {void}
+     */
+    set(key, value) {
+        localStorage.setItem(key, value);
+    }
+
+    /** 
+     * @method get
+     * @description - get a  key-value pair Item from the the localStorage
+     * @param {sting} key - to find the correct localStorage item
+     * @return {string}
+     */
+    get(key) {
+        localStorage.getItem(key);
+    }
+
+    /** 
+     * @method remove
+     * @description - removes a localstorage Item 
+     * @param {sting} key - to find the correct localStorage item
+     * @return {void}
+     */
+    remove(key) {
+        localStorage.removeItem(key);
+    }
+
+    /** 
+     * @method isset
+     * @description - check if localStorage Item is set
+     * @param {sting} key - to find the correct localStorage item
+     * @return {boolean}
+     */
+    isset(key) {
+        return localStorage.getItem(key) !== null;
+    }
+
+    // Work with Arrays
+
+    /** 
+     * @method setArray
+     * @description - set a Array as a localStorage Item
+     * @param {sting} key - the localStorage Key
+     * @param {array} array - the array to be stored
+     * @return {void}
+     */
+    setArray(key, array) {
+        if (Array.isArray(array)) {
+            localStorage.setItem(key, JSON.stringify(array));
+        }
+    }
+
+    /** 
+     * @method getArray
+     * @description - get the Array
+     * @param {sting} key - the localStorage Key
+     * @return {array}
+     */
+    getArray(key) {
+        return JSON.parse(localStorage.getItem(key)) ?? [];
+    }
+
+    /** 
+     * @method addToArray
+     * @description - added a new Value to the Array
+     * @param {sting} key - the localStorage Key
+     * @param {sting|number} value - to added value
+     * @return {void}
+     */
+    addToArray(key, value) {
+        const array = JSON.parse(localStorage.getItem(key)) ?? [];
+        array.push(value);
+        if (Array.isArray(array)) {
+            localStorage.setItem(key, JSON.stringify(array));
+        }
+    }
+
+    /** 
+     * @method removeFromArray
+     * @description - remove a Value from the Array
+     * @param {sting} key - the localStorage Key
+     * @param {sting|number} value - to removed value
+     * @return {void}
+     */
+    removeFromArray(key, value) {
+        const array = JSON.parse(localStorage.getItem(key)) ?? [];
+        const newArray = [];
+
+        array.forEach(element => {
+            if (element !== value) {
+                newArray.push(element)
+            }
+        });
+        localStorage.setItem(key, JSON.stringify(newArray));
+    }
+
+    /** 
+     * @method updateArray
+     * @description - updates a value of the array
+     * @param {sting} key - the localStorage Key
+     * @param {any} oldvalue - the current index value of the array
+     * @param {any} newvalue - the value with which the old index value is to be replaced
+     * @return {void}
+     */
+    updateArray(key, oldvalue, newvalue) {
+        const array = JSON.parse(localStorage.getItem(key)) ?? [];
+        const newArray = [];
+        array.forEach(element => {
+            if (element === oldvalue) {
+                newArray.push(newvalue);
+            } else {
+                newArray.push(element);
+            }
+        });
+
+        if (Array.isArray(newArray)) {
+            localStorage.setItem(key, JSON.stringify(newArray));
+        }
+    }
+
+    /**
+     * @method isIntoArray
+     * @description - checks if value is present in the array
+     * @param {string} key - the localStorage Key
+     * @param {any} value - the value to be checked
+     * @returns {boolean}  
+     */
+    isIntoArray(key, value) {
+        const array = JSON.parse(localStorage.getItem(key)) ?? [];
+        return array.includes(value);
+    }
+
+    // Work with Objects
+
+    /**
+    * @method setObject
+    * @description - sets an object as a localstorage item
+    * @param {string} key - the localStorage Key
+    * @param {object} object - the object to be set
+    * @returns {boolean}  
+    */
+    setObject(key, object) {
+        localStorage.setItem(key, JSON.stringify([object]));
+    }
+
+    /**
+    * @method getObjects
+    * @description - returns the objects wrap into an array
+    * @param {string} key - the localStorage Key
+    * @returns {array}  
+    */
+    getObjects(key) {
+        return JSON.parse(localStorage.getItem(key)) ?? [];
+    }
+
+    /**
+    * @method addObject
+    * @description - add an Object to localStorage Item
+    * @param {string} key - the localStorage Key
+    * @param {object} object - the new Object 
+    * @returns {void}  
+    */
+    addObject(key, object) {
+        const array = JSON.parse(localStorage.getItem(key)) ?? [];
+        array.push(object);
+        localStorage.setItem(key, JSON.stringify(array));
+    }
+
+    /**
+    * @method removeObject
+    * @description - removed an Object from localStorage Item
+    * @param {string} key - the localStorage Key
+    * @param {string} objectKey - the key of the object to be deleted
+    * @param {any} value - the value of the object key to be deleted
+    * @returns {void}  
+    */
+    removeObject(key, objectKey, value) {
+
+        const array = JSON.parse(localStorage.getItem(key)) ?? [];
+        const newArray = [];
+
+        array.forEach(obj => {
+            if (obj[objectKey] !== value) {
+                newArray.push(obj);
+            }
+        });
+
+        localStorage.setItem(key, JSON.stringify(newArray));
+    }
+
+    // Storage Infos
+
+    /**
+     * @method supported
+     * @description - checks if the browser supports localstorage
+     * @param {string} key - the localStorage Key
+     * @returns {boolean}  
+     */
+     supported() {
+        let check = 'check';
+        try {
+            localStorage.setItem(check, check);
+            localStorage.removeItem(check);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    // Lists all used Keys
+    getAllKeys() {
+        const keys = [];
+        for (let name in localStorage) {
+            if (!localStorage.hasOwnProperty(name)) {
+                continue;
+            }
+            keys.push(name);
+        }
+        return keys;
+    }
+
+    // Clears all localStorage from Browser
+    clear() {
+        localStorage.clear();
+    }
+
+    // Overview about the used and free space at the localStorage
+    space() {
+        const maxSpace = 5 * 1024 * 1024; // 5MB => 5242880 Bytes
+        let usedSpace = 0; // 0 Bytes
+        let freeSpaceInProcent;
+        let freeSpaceInMB;
+        let usedSpaceInMB;
+        let usedSpaceInProcent;
+
+        function format(float) {
+            return parseFloat(Number(float).toFixed(6))
+        }
+
+        for (let name in localStorage) {
+            if (!localStorage.hasOwnProperty(name)) {
+                continue;
+            }
+            let size = (localStorage[name].length + name.length) * 2;
+            usedSpace += size;
+        }
+
+        usedSpaceInProcent = format((usedSpace / maxSpace * 100));
+        usedSpaceInMB = format(usedSpace / 1024 / 1024);
+
+        freeSpaceInProcent = format(100 - usedSpaceInProcent);
+        freeSpaceInMB = format((maxSpace - usedSpace) / 1024 / 1024);
+
+        return {
+            free: {
+                mb: freeSpaceInMB,
+                procent: freeSpaceInProcent
+            }, used: {
+                mb: usedSpaceInMB,
+                procent: usedSpaceInProcent
+            }
+        };
+    }
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+# My Localstorage
+## Description
+A tool that helps you store data in the LocalStorage of your browser.
+It can be used when you want to build a small application that works offline and does not require a large database to store data.
+
+## Getting Started
+### Installation
+...
+
+## Usage
+...
+
+## License
+Distributed under the MIT License. See <u>LICENCE.txt</u> for more information.
+
+## Contact
+hallo@jpbehrens.de
